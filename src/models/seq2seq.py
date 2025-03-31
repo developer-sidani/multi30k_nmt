929bdac33ebc975af6103cb1b99361180d7c8909
developer-sidani <developer.sidani@gmail.com>
2025-03-31 12:08:44 +0200

    fix: tokenizer

--- src/models/seq2seq.py
+++ src/models/seq2seq.py
@@ -6,5 +6,5 @@ def load_seq2seq_model(model_name_or_path: str, src_lang: str, tgt_lang: str):
     `src_lang` and `tgt_lang` should be full language codes (e.g., "en_XX", "de_DE", "fr_XX", "cs_CZ").
     """
     model = MBartForConditionalGeneration.from_pretrained(model_name_or_path)
-    tokenizer = MBart50TokenizerFast.from_pretrained(model_name_or_path, src_lang=src_lang, tgt_lang=tgt_lang)
+    tokenizer = MBart50TokenizerFast.from_pretrained(model_name_or_path)
     return model, tokenizer
